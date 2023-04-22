e20bd02469c9d20eafd08842d83a31b0e12b5433
joão lorenzo malutta haidar <lorenzo.malutta@gmail.com>
2023-04-21 21:01:32 -0300

    move laravel to server and react js + vite to client

---
+++ client/src/main.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Home />,
+    },
+    {
+        path: "/login",
+        element: <Login />,
+    },
+    {
+        path: "/Register",
+        element: <Register />,
+    },
+    {
+        path: "/Todo",
+        element: <Dashboard />,
+    },
+]);
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>,
+)
